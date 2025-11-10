9eed28020d84911f4bae1bbb420afb1ad34e649f
Bibek <bbk.mhrzn@gmail.com>
2025-11-09 22:36:33 -0500

    sticker to csv cmnt

--- sticker_to_dvp_csv.py
+++ sticker_to_dvp_csv.py
@@ -267,3 +267,19 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+
+'''
+I ran it in terminal:
+
+python3 sticker_to_dvp_csv.py \
+  --png images/cat.jpg \
+  --out_csv sticker_encoded2.csv \
+  --map PCLK=0 HREF=5 D0=6 D1=3 D2=4 D3=1 D4=2 D5=8 D6=9 D7=10 VSYNC=7 \
+  --layout rgb \
+  --byte_order lsb_first \
+  --bit_order normal \
+  --sample_rate_mhz 20 \
+  --meta_prefix
+'''
